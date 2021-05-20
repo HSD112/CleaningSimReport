f3b4d8b6e6256a6b64359059a85cde062d30c95f
HSD112 <horvathstefandaniel@gmail.com>
2021-05-20 18:24:54 +0200

    Rest api testing

---
+++ CleaningSimReport/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleaningSimReport.Models
+{
+    public class User
+    {
+        public string uUser_id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+    }
+}
