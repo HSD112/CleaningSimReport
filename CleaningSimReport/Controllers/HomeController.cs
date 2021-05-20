f15d2dcb3b1cab473551b5e9a016a0b6d650bf02
HSD112 <horvathstefandaniel@gmail.com>
2021-05-20 03:34:10 +0200

    SQL connection and data display done

--- CleaningSimReport/Controllers/HomeController.cs
+++ CleaningSimReport/Controllers/HomeController.cs
@@ -7,23 +7,74 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
+using System.Data.SqlClient;
+using Microsoft.Extensions.Logging;
+using CleaningSimReport.Models;
+
 namespace CleaningSimReport.Controllers
 {
     public class HomeController : Controller
     {
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+        SqlConnection con = new SqlConnection();
+
+        List<Report> reports = new List<Report>();
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            con.ConnectionString = CleaningSimReport.Properties.Resources.ConnectionString;
         }
 
         public IActionResult Index()
         {
-            return View();
+            FetchData();
+            return View(reports);
         }
 
-        public IActionResult Privacy()
+        private void FetchData()
+        {
+            if (reports.Count > 0)   //prevent displaying duplication
+            {
+                reports.Clear();
+            }
+            try
+            {
+                con.Open();
+                com.Connection = con;
+                com.CommandText = "SELECT TOP (1000) " +
+                    "[report_id]," +
+                    "[user_id]," +
+                    "[time_seconds]," +
+                    "[mistakes_count]," +
+                    "[level_name] " +
+                    "FROM[dbo].[Report]";
+                dr = com.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    reports.Add(new Report()
+                    {
+                        report_id = dr["report_id"].ToString(),
+                        user_id = dr["user_id"].ToString(),
+                        time_seconds = dr["time_seconds"].ToString(),
+                        mistakes_count = dr["mistakes_count"].ToString(),
+                        level_name = dr["level_name"].ToString()
+                    });
+                }
+
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public IActionResult About()
         {
             return View();
         }
---
+++ CleaningSimReport/Models/Report.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleaningSimReport.Models
+{
+    public class Report
+    {
+        public string report_id { get; set; }
+        public string user_id { get; set; }
+        public string time_seconds { get; set; }
+        public string mistakes_count { get; set; }
+        public string level_name { get; set; }
+    }
+}
