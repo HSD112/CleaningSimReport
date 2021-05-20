f125d6a12e16a5adfe4e62425854b16606e0ff87
HSD112 <horvathstefandaniel@gmail.com>
2021-05-20 02:40:47 +0200

    Testing automation

---
+++ CleaningSimReport/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CleaningSimReport.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
