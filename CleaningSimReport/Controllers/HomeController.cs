216ace99b8b1cacfa970f56d8a3f76e545d37954
HSD112 <horvathstefandaniel@gmail.com>
2021-05-27 20:02:49 +0200

    FinalCleanup

--- CleaningSimReport/Controllers/HomeController.cs
+++ CleaningSimReport/Controllers/HomeController.cs
@@ -4,9 +4,6 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-
 using System.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 
--- CleaningSimReport/Models/User.cs
+++ CleaningSimReport/Models/User.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace CleaningSimReport.Models
+﻿namespace CleaningSimReport.Models
 {
     public class User
     {
